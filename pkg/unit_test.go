83e664ae325bc35873f5933ed42e23c57d9793e5
DamirTB <talipdamir@gmail.com>
2024-06-04 14:34:12 +0500

    added unit tests for games

--- pkg/unit_test.go
+++ pkg/unit_test.go
@@ -28,15 +28,13 @@ func TestInsertGame(t *testing.T) {
 		Price: 1000,
 		Genres: []string{"Adventure", "Horror"},
 	}
-
-
 	mock.ExpectQuery(`INSERT INTO games`).
 		WithArgs(game.Name, game.Price, pq.Array(game.Genres)).
 		WillReturnRows(sqlmock.NewRows([]string{"id", "created_at", "version"}).AddRow(1, time.Now(), 1))
 
 	err = app.Models.Games.Insert(game)
 	if err != nil {
-		t.Errorf("unexpected error inserting movie: %s", err)
+		t.Errorf("unexpected error inserting games: %s", err)
 	}
 
 	if err := mock.ExpectationsWereMet(); err != nil {
@@ -44,30 +42,57 @@ func TestInsertGame(t *testing.T) {
 	}
 }
 
-// func TestGetGame(t *testing.T) {
-// 	db, mock, err := sqlmock.New()
-// 	if err != nil {
-// 		t.Fatalf("error creating mock database: %s", err)
-// 	}
-// 	defer db.Close()
-// 	app := &Application{
-// 		Models: data.NewModels(db),
-//     }
-// 	mock.ExpectQuery(`SELECT * FROM movies WHERE id = $1`).
-// 		WithArgs(1).
-// 		WillReturnRows(sqlmock.NewRows([]string{"id", "created_at", "name", "price", "genres", "version"}).
-// 			AddRow(1, time.Now(), "Warcraft", 1000, pq.Array([]string{"Adventure", "Horror"}), 1))
+func TestGetGame(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("error creating mock database: %s", err)
+	}
+	defer db.Close()
 
-// 	movie, err := app.Models.Games.Get(1)
-// 	if err != nil {
-// 		t.Errorf("unexpected error fetching movie: %s", err)
-// 	}
+	app := &Application{
+		Models: data.NewModels(db),
+	}
 
-// 	if movie.ID != 1 {
-// 		t.Errorf("incorrect movie ID, got %d, want %d", movie.ID, 1)
-// 	}
+	mock.ExpectQuery(`SELECT \* FROM games WHERE`).
+		WithArgs(1).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "created_at", "name", "price", "genres", "version"}).
+			AddRow(1, time.Now(), "Warcraft", 1000, pq.Array([]string{"Adventure", "Horror"}), 1))
 
-// 	if err := mock.ExpectationsWereMet(); err != nil {
-// 		t.Errorf("there were unfulfilled expectations: %s", err)
-// 	}
-// }
+	game, err := app.Models.Games.Get(1)
+	if err != nil {
+		t.Errorf("unexpected error retrieving game: %s", err)
+	}
+	if game.ID != 1 {
+		t.Errorf("incorrect game ID, got %d want %d", game.ID, 1)
+	}
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestDeleteGame(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("error creating mock database: %s", err)
+	}
+	defer db.Close()
+
+	app := &Application{
+		Models: data.NewModels(db),
+	}
+
+	mock.ExpectExec(`DELETE FROM games WHERE`).
+		WithArgs(1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err = app.Models.Games.Delete(1)
+	if err != nil {
+		t.Errorf("unexpected error deleting game: %s", err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+
